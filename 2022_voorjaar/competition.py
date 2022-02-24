9f6a44f2421017197f0a4f7c533b0d70d53b20f5
Mike Fleuren <mikefleuren@gmail.com>
2022-02-24 22:11:55 +0100

    Changed csv loading structure

--- 2022_voorjaar/competition.py
+++ 2022_voorjaar/competition.py
@@ -1,16 +1,26 @@
 import pandas as pd
 
+import os
 import sys
 sys.path.append('.')
 import webscraper
+import import_files
 
 BASE_URL = 'https://www.procyclingstats.com/race/'
 YEAR = 2021
 BASE_EPITHET = 'result'
 
-selected_matches = pd.read_csv('matches.csv', sep=';')
+PATH_CURRENT_FILE = os.path.dirname(os.path.realpath(__file__)) 
+PATH_CSV_TEAMS = os.path.join(PATH_CURRENT_FILE, 'teams.csv')
+PATH_CSV_MATCHES = os.path.join(PATH_CURRENT_FILE, 'matches.csv')
+PATH_CSV_POINTS = os.path.join(PATH_CURRENT_FILE, 'points.csv')
 
-for url_epi in selected_matches['url_epithet']:
+# Load CSV-files with teams, match and points information
+df_teams = import_files.import_teams(PATH_CSV_TEAMS)
+df_matches = import_files.import_matches(PATH_CSV_MATCHES)
+df_points = import_files.import_points(PATH_CSV_POINTS)
+
+for url_epi in df_matches['url_epithet']:
 
     url = BASE_URL + url_epi + '/' + str(YEAR) + '/' + BASE_EPITHET
 
--- teams.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import pathlib
-from typing import Union
-
-def import_teams(path: Union[str, pathlib.Path]):
-    return pd.read_csv(path, sep=';')
-
-teams = import_teams('2022_voorjaar/teams.csv')
