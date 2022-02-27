c63c778a392e89221d05bbc612812b216577c32c
Mike Fleuren <mikefleuren@gmail.com>
2022-02-27 21:58:50 +0100

    Calculate scores in main.py and print in console

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 from datetime import datetime
 import time
+import pandas as pd
 from scraper_pcs.import_files import load_csv_files
 from scraper_pcs.webscraper import scrape_website
+from scraper_pcs.calculate_scores import calculate_match_points, calculate_stage_result
 
 
 COMPETITION_NAME = 'Voorjaar'
@@ -15,6 +17,9 @@ CURRENT_DATE = datetime.now()
 path_csv_files = os.path.join(os.path.dirname(os.path.realpath(__file__)), COMPETITION_YEAR_NAME) 
 df_teams, df_matches, df_points = load_csv_files(path_csv_files)
 
+# Pre-allocate all_results
+all_results = pd.DataFrame()
+
 # Select the matches to scrape
 matches_to_scrape = df_matches[df_matches['MATCH_DATE'] <= CURRENT_DATE]
 
@@ -22,11 +27,16 @@ for row in matches_to_scrape.iterrows():
     match = row[1]
 
     print(match['MATCH'])
-    try:
-        stage_results = scrape_website(match)
-        stage_results.to_csv('temp/results_example.csv', index=False)        
+    stage_results = scrape_website(match)
+    match_points = calculate_match_points(stage_results, df_points, df_teams)
+    stage_standing = calculate_stage_result(match_points)
 
-    except:
-        print(f"{match['MATCH']} not working")
+    print(stage_standing['POINTS'])
+
+    all_results = all_results.append(match_points)
+
 
     time.sleep(1)
+
+print('Algemeen klassement')
+print(all_results.groupby('COACH')['POINTS'].sum().sort_values(ascending=False))
--- scraper_pcs/calculate_scores.py
+++ scraper_pcs/calculate_scores.py
@@ -1,12 +1,22 @@
-from unittest import result
 import pandas as pd
 
-def calculate_match_points(result_df: pd.DataFrame, points_df: pd.DataFrame) -> pd.DataFrame:
-    """Calculate points per match"""
-    joined_df = result_df.join(points_df.set_index('RNK'), on='RNK', how='left')
-    joined_df['POINTS'] = (joined_df['MATCH_LEVEL'] * joined_df['POINTS_RAW']).astype('int')
-    joined_df.drop('POINTS_RAW', axis=1, inplace=True)
+def calculate_match_points(result_df: pd.DataFrame, points_df: pd.DataFrame, teams_df: pd.DataFrame) -> pd.DataFrame:
+    """Calculate points per match by joining match results, points and coached teams"""
+
+    join_result_points = result_df.join(points_df.set_index('RNK'), on='RNK', how='left')
+    join_result_points['POINTS'] = (join_result_points['MATCH_LEVEL'] * join_result_points['POINTS_RAW']).astype('int')
+    joined_df = join_result_points.join(teams_df.set_index('RIDER'), on='RIDER', how='left')
+    joined_df.drop(['ROUND', 'PICK', 'POINTS_RAW'], axis=1, inplace=True)
     return joined_df
+
+def calculate_stage_result(match_points: pd.DataFrame) -> pd.DataFrame:
+    """Group match result by coach, return ordered dataframe with points by coach"""
+
+    stage_result = pd.DataFrame(data=match_points.groupby('COACH')['POINTS'].sum().sort_values(ascending=False))
+    stage_result['MATCH'] = match_points.loc[0, 'MATCH']
+    stage_result['MATCH_LEVEL'] = match_points.loc[0, 'MATCH_LEVEL']
+
+    return stage_result
 
 
 if __name__ == '__main__':
@@ -14,6 +24,7 @@ if __name__ == '__main__':
     points_df = pd.read_csv('./2022_Voorjaar/points.csv', sep=';')
     teams_df = pd.read_csv('./2022_Voorjaar/teams.csv', sep=';')
     
-    match_points = calculate_match_points(result_df, points_df)
+    match_points = calculate_match_points(result_df, points_df, teams_df)
+    stage_result = calculate_stage_result(match_points)
+    print(stage_result.head())
 
-    print(match_points.head())
