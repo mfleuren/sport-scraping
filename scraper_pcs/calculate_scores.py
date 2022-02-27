700cdd2d1e94e272ebe7b77aa000a8b1ce65a198
Mike Fleuren <mikefleuren@gmail.com>
2022-02-27 21:34:38 +0100

    Calculate points by match

--- scraper_pcs/calculate_scores.py
+++ scraper_pcs/calculate_scores.py
@@ -1,11 +1,19 @@
+from unittest import result
 import pandas as pd
 
-
+def calculate_match_points(result_df: pd.DataFrame, points_df: pd.DataFrame) -> pd.DataFrame:
+    """Calculate points per match"""
+    joined_df = result_df.join(points_df.set_index('RNK'), on='RNK', how='left')
+    joined_df['POINTS'] = (joined_df['MATCH_LEVEL'] * joined_df['POINTS_RAW']).astype('int')
+    joined_df.drop('POINTS_RAW', axis=1, inplace=True)
+    return joined_df
 
 
 if __name__ == '__main__':
     result_df = pd.read_csv('./temp/results_example.csv')
-    points_df = pd.read_csv('./2022_Voorjaar/points.csv')
-    teams_df = pd.read_csv('./2022_Voorjaar/teams.csv')
+    points_df = pd.read_csv('./2022_Voorjaar/points.csv', sep=';')
+    teams_df = pd.read_csv('./2022_Voorjaar/teams.csv', sep=';')
     
-    print(result_df.head())
+    match_points = calculate_match_points(result_df, points_df)
+
+    print(match_points.head())
