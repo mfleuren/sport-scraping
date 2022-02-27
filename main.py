66123ceb3b5cd984595e67887a0efc94cafe4761
Mike Fleuren <mikefleuren@gmail.com>
2022-02-27 21:16:36 +0100

    Added match info to results

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import os
 from datetime import datetime
+import time
 from scraper_pcs.import_files import load_csv_files
 from scraper_pcs.webscraper import scrape_website
+
 
 COMPETITION_NAME = 'Voorjaar'
 COMPETITION_YEAR = 2022
 COMPETITION_YEAR_NAME = str(COMPETITION_YEAR) + '_' + COMPETITION_NAME
-
-BASE_URL = 'https://www.procyclingstats.com/race/'
-YEAR = datetime.now().year
-BASE_EPITHET = 'result'
 
 CURRENT_DATE = datetime.now()
 
@@ -20,15 +18,15 @@ df_teams, df_matches, df_points = load_csv_files(path_csv_files)
 # Select the matches to scrape
 matches_to_scrape = df_matches[df_matches['MATCH_DATE'] <= CURRENT_DATE]
 
-for url_epi in [matches_to_scrape.iloc[0]['URL_EPITHET']]:
+for row in matches_to_scrape.iterrows():
+    match = row[1]
 
-    url = BASE_URL + url_epi + '/' + str(YEAR) + '/' + BASE_EPITHET
-
-    print(url)
-
+    print(match['MATCH'])
     try:
-        stage_results = scrape_website(url)
-        stage_results.to_csv('temp/results_example.csv')        
+        stage_results = scrape_website(match)
+        stage_results.to_csv('temp/results_example.csv', index=False)        
 
     except:
-        print(f"{url} not working")
+        print(f"{match['MATCH']} not working")
+
+    time.sleep(1)
