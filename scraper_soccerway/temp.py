167e03bc293d9ee86791de0f37f8d26dd6ad4631
Mike Fleuren <mikefleuren@gmail.com>
2022-05-24 22:36:34 +0200

    Implemented testing using local file

--- scraper_soccerway/temp.py
+++ scraper_soccerway/temp.py
@@ -3,10 +3,20 @@ import requests
 from lxml import html
 from typing import List
 
-URL = 'https://nl.soccerway.com/matches/2022/05/15/netherlands/eredivisie/sbv-vitesse/afc-ajax/3512762/'
-REQUEST_HEADERS = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0"}
-result = requests.get(URL, headers=REQUEST_HEADERS)
-tree = html.fromstring(result.text)
+def load_html(from_file: bool = False) -> str:
+    if not from_file:
+        URL = 'https://nl.soccerway.com/matches/2022/05/15/netherlands/eredivisie/sbv-vitesse/afc-ajax/3512762/'
+        REQUEST_HEADERS = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0"}
+        result = requests.get(URL, headers=REQUEST_HEADERS)
+        with open('test_html.txt', 'w', encoding='utf-8') as f:
+            f.write(result.text)
+        return result.text
+    else:
+        import codecs
+        f = codecs.open('test_html.txt', 'r', encoding='utf-8')
+        return f.read()
+    
+tree = html.fromstring(load_html(from_file=True))
 
 def extract_url_by_class(level:str, class_str:str) -> List[str]:
     return tree.xpath(f'//{level}[@class="{class_str}"]/@href')
@@ -14,17 +24,12 @@ def extract_url_by_class(level:str, class_str:str) -> List[str]:
 
 def extract_txt_by_class(level:str, class_str:str) -> List[str]:
     txt = tree.xpath(f'//{level}[@class="{class_str}"]/text()')
-    txt_cleaned = [string.replace(' ', '').replace('\n', '') for string in txt]
+    txt_cleaned = [string.replace(' ', '').replace('\n', '').replace('\r', '') for string in txt]
     return list(filter(None, txt_cleaned))
 
 
-# club_urls = extract_url_by_class('a', 'team-title')
-# club_names = extract_txt_by_class('a', 'team-title')
-# home_team = club_names[0]
-# away_team = club_names[1]
-# print(home_team, away_team)
-
+club_urls = extract_url_by_class('a', 'team-title')
 match_state = extract_txt_by_class('span', 'match-state')
 html_score = extract_txt_by_class('h3', 'thick scoretime')
 
-print(match_state, html_score)
+print(club_urls, html_score)
