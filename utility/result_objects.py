51633bdcdc9a56ea05f295ee659e728425424e5e
Mike Fleuren <mikefleuren@gmail.com>
2022-08-07 22:19:49 +0200

    Started initializing CompetitionData result_object

--- utility/result_objects.py
+++ utility/result_objects.py
@@ -3,12 +3,45 @@ import pandas as pd
 import numpy as np
 import os
 from dotenv import load_dotenv
+from typing import Union
 
+"""
+SET CONSTANTS
+"""
 
 load_dotenv()
-PATH_INPUT = os.path.join(os.getcwd(), 'inputs', f"{os.getenv('COMPETITION_YEAR')}_{os.getenv('COMPETITION_NAME')}")
-PATH_RESULTS = os.path.join(os.getcwd(), 'results', f"{os.getenv('COMPETITION_YEAR')}_{os.getenv('COMPETITION_NAME')}")
+# Cycling input
+PATH_INPUT = os.path.join(
+    os.getcwd(), 
+    'inputs', 
+    f"{os.getenv('COMPETITION_YEAR')}_{os.getenv('COMPETITION_NAME')}"
+    )
+PATH_RESULTS = os.path.join(
+    os.getcwd(), 'results', 
+    f"{os.getenv('COMPETITION_YEAR')}_{os.getenv('COMPETITION_NAME')}"
+    )
 
+# Football input
+FOOTBALL_PATH_INPUT = os.path.join(
+    os.getcwd(), 
+    'inputs', 
+    f"{os.getenv('FOOTBALL_COMPETITION_YEAR')}_{os.getenv('FOOTBALL_COMPETITION_NAME')}"
+    )
+FOOTBALL_PATH_RESULTS = os.path.join(
+    os.getcwd(), 
+    'results', 
+    f"{os.getenv('FOOTBALL_COMPETITION_YEAR')}_{os.getenv('FOOTBALL_COMPETITION_NAME')}"
+    )
+
+FILE_FOOTBALL_TEAMS_INPUT = os.path.join(FOOTBALL_PATH_INPUT, 'teams.csv')
+FILE_FOOTBALL_TEAMS_RESULTS = os.path.join(FOOTBALL_PATH_RESULTS, 'teams.csv')
+FILE_FOOTBALL_SUBSTITUTIONS = os.path.join(FOOTBALL_PATH_INPUT, 'substitutions.csv')
+FILE_FOOTBALL_POINTS_SCHEME = os.path.join(FOOTBALL_PATH_INPUT, 'points_scheme.csv')
+
+
+"""
+DATACLASSES FOR CYCLING SCRAPERS
+"""
 
 @dataclass
 class Message:
@@ -77,3 +110,55 @@ class StageResults:
     def append_stages_to_existing_data(self):
         self.all_results = pd.concat([self.all_results, pd.concat(self.stage_results)], ignore_index=True)
         self.all_points = pd.concat([self.all_points, pd.concat(self.stage_points)], ignore_index=True)
+
+
+"""
+DATACLASSES FOR FOOTBALL SCRAPERS
+"""
+
+@dataclass
+class CompetitionData:
+    chosen_teams: pd.DataFrame = field(init=False)
+    substitutions: pd.DataFrame = field(init=False)
+    points_scheme: pd.DataFrame = field(init=False)
+
+    
+
+    def __post_init__(self):
+        self.chosen_teams = self.load_file_from_input_or_results(FILE_FOOTBALL_TEAMS_INPUT, FILE_FOOTBALL_TEAMS_RESULTS)
+        self.substitutions = pd.read_csv(FILE_FOOTBALL_SUBSTITUTIONS, sep=';')
+        self.points_scheme = pd.read_csv(FILE_FOOTBALL_POINTS_SCHEME, sep=';')
+
+
+    def load_file_from_input_or_results(self, 
+        input_path: Union[str, os.PathLike], 
+        result_path: Union[str, os.PathLike]
+        ) -> pd.DataFrame:
+        """
+        Logic to determine which teams file to initialize:
+        - If results file exists: use results
+        - Else if input file exists: use inputs
+        - Else: Error
+        """
+
+        try:
+            if os.path.exists(result_path):
+                df = pd.read_csv(result_path, sep=';')
+                print(f'Loaded file {result_path} from result.')
+                return df
+            elif os.path.exists(input_path):
+                df = pd.read_csv(input_path, sep=';')
+                print(f'Loaded file {input_path} from input.')
+                return df
+            else:
+                raise Exception(input_path, result_path)
+        except Exception:
+            print(f'Unexpected Exception. Files {input_path} and {result_path} both not found.')
+            raise
+
+        
+
+if __name__ == '__main__':
+    data = CompetitionData()
+    print(data.points_scheme.columns)
+
