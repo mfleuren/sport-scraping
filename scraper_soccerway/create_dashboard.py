627e66219752b0d68bfb139b20bba791511fbd11
Mike Fleuren <mikefleuren@gmail.com>
2022-09-19 22:11:58 +0200

    Add tabs to dashboard

--- scraper_soccerway/create_dashboard.py
+++ scraper_soccerway/create_dashboard.py
@@ -10,6 +10,10 @@ def main() -> None:
     data = pd.read_csv('.\\.\\results\\2022_Eredivisie\\points_coach.csv', sep=';')
 
     app = Dash(external_stylesheets=[BOOTSTRAP])
+
+    # App settings
+    app.config.suppress_callback_exceptions=True #Required to surpress errors with tabs
+
     app.title = 'WZV League Dashboard'
     app.layout = create_layout(app, data)
     app.run_server(debug=True)
--- scraper_soccerway/dashboard/components/layout.py
+++ scraper_soccerway/dashboard/components/layout.py
@@ -1,11 +1,38 @@
-from dash import Dash, html
+from dash import Dash, html, dcc
+from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import pandas as pd
 
-from . import bar_chart_round, bar_chart_gc, round_dropdown, coaches_dropdown
+from . import bar_chart_round, bar_chart_gc, round_dropdown, coaches_dropdown, ids
 
 
 def create_layout(app: Dash, data: pd.DataFrame) -> html.Div:
+
+    @app.callback(
+        Output(ids.TAB_CONTENTS, 'children'),
+        [Input(ids.TAB_TABS, 'value')]
+        )
+    def update_tab_content(tab: str) -> list:
+
+        bar_chart_rnd = bar_chart_round.render(app, data)
+        bar_chart_gen = bar_chart_gc.render(app, data)
+
+        if tab == ids.TAB_STANDINGS: 
+            print('in tab standings')
+            return [
+                dbc.Col(bar_chart_rnd),
+                dbc.Col(bar_chart_gen)
+                ]
+                
+        elif tab == ids.TAB_TEAMS:
+            print('in tab teams')
+            return []
+        elif tab == ids.TAB_STATS:
+            print('in tab stats')
+            return []
+        else:
+            return []
+
     return html.Div(
         className ="app-div", 
         children = [
@@ -23,12 +50,16 @@ def create_layout(app: Dash, data: pd.DataFrame) -> html.Div:
                     )
                 ]
             ),
-            dbc.Row(
+            dcc.Tabs(
+                id=ids.TAB_TABS, 
+                value=ids.TAB_STANDINGS, 
                 children=[
-                    dbc.Col(bar_chart_round.render(app, data)),
-                    dbc.Col(bar_chart_gc.render(app, data))
-                ]
-            )
-            
+                    dcc.Tab(label='Standings', value=ids.TAB_STANDINGS),
+                    dcc.Tab(label='Teams', value=ids.TAB_TEAMS),
+                    dcc.Tab(label='Stats', value=ids.TAB_STATS),
+                    ]
+                ),
+            dbc.Row(id=ids.TAB_CONTENTS)
+                        
         ]
     )
