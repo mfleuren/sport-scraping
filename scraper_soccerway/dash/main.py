453b8832f3069e09c453118b14049af2817d0f93
Mike Fleuren <mikefleuren@gmail.com>
2023-01-23 22:28:53 +0100

    Small change to main

--- scraper_soccerway/dash/main.py
+++ scraper_soccerway/dash/main.py
@@ -4,12 +4,11 @@ import dash_bootstrap_components as dbc
 from scraper_soccerway.dash.src.components.layout import create_layout
 from scraper_soccerway.dash.src.data.loader import load_points_by_coach_data, load_points_by_player_data
 
-if __name__ == "__main__":
-    teams_data = load_points_by_coach_data("scraper_soccerway/dash/data/")
-    players_data = load_points_by_player_data("scraper_soccerway/dash/data/")
 
-    app = Dash(external_stylesheets=[dbc.themes.GRID])
-    server = app.server
-    app.title = "WZV League 2022-2023"
-    app.layout = create_layout(app, teams_data, players_data)
-    app.run()
+teams_data = load_points_by_coach_data("scraper_soccerway/dash/data/")
+players_data = load_points_by_player_data("scraper_soccerway/dash/data/")
+
+app = Dash(external_stylesheets=[dbc.themes.GRID])
+app.title = "WZV League 2022-2023"
+app.layout = create_layout(app, teams_data, players_data)
+app.run()
