4b64ca8bd2252ecc242a5d585ce3239ee8d1c458
Mike Fleuren <mikefleuren@gmail.com>
2023-01-23 22:12:36 +0100

    Added dash data to git

--- scraper_soccerway/dash/main.py
+++ scraper_soccerway/dash/main.py
@@ -4,8 +4,8 @@ import dash_bootstrap_components as dbc
 from scraper_soccerway.dash.src.components.layout import create_layout
 from scraper_soccerway.dash.src.data.loader import load_points_by_coach_data, load_points_by_player_data
 
-teams_data = load_points_by_coach_data("././results/2022_Eredivisie/points_coach.csv")
-players_data = load_points_by_player_data("././results/2022_Eredivisie/")
+teams_data = load_points_by_coach_data("scraper_soccerway/dash/data/")
+players_data = load_points_by_player_data("scraper_soccerway/dash/data/")
 
 app = Dash(external_stylesheets=[dbc.themes.GRID])
 app.title = "WZV League 2022-2023"
--- scraper_soccerway/dash/src/data/loader.py
+++ scraper_soccerway/dash/src/data/loader.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-def load_points_by_coach_data(path: str) -> pd.DataFrame:
+def load_points_by_coach_data(folder: str) -> pd.DataFrame:
     """Load data from a csv file"""
 
-    data = pd.read_csv(path, sep=';')
+    data = pd.read_csv(folder + "points_coach.csv", sep=';')
 
     data["P_Totaal"] = data["P_Totaal"].round(2)
 
