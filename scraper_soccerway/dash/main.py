10cc439fae0424884a116341b8242725fb302543
Mike Fleuren <mikefleuren@gmail.com>
2023-01-23 22:08:55 +0100

    Remove def main from main

--- scraper_soccerway/dash/main.py
+++ scraper_soccerway/dash/main.py
@@ -4,17 +4,10 @@ import dash_bootstrap_components as dbc
 from scraper_soccerway.dash.src.components.layout import create_layout
 from scraper_soccerway.dash.src.data.loader import load_points_by_coach_data, load_points_by_player_data
 
+teams_data = load_points_by_coach_data("././results/2022_Eredivisie/points_coach.csv")
+players_data = load_points_by_player_data("././results/2022_Eredivisie/")
 
-def main() -> None:
-
-    teams_data = load_points_by_coach_data("././results/2022_Eredivisie/points_coach.csv")
-    players_data = load_points_by_player_data("././results/2022_Eredivisie/")
-
-    app = Dash(external_stylesheets=[dbc.themes.GRID])
-    app.title = "WZV League 2022-2023"
-    app.layout = create_layout(app, teams_data, players_data)
-    app.run()
-
-
-if __name__ == "__main__":
-    main()
+app = Dash(external_stylesheets=[dbc.themes.GRID])
+app.title = "WZV League 2022-2023"
+app.layout = create_layout(app, teams_data, players_data)
+app.run()
