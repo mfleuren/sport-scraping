659160282c718eda053339ef3a97bb91df545c29
Mike Fleuren <mikefleuren@gmail.com>
2023-02-03 22:06:28 +0100

    Convert part of the changes to main

--- scraper_soccerway/dash/main.py
+++ scraper_soccerway/dash/main.py
@@ -5,15 +5,11 @@ from scraper_soccerway.dash.src.data.loader import DashData
 from scraper_soccerway.dash.src.components.layout import create_layout
 
 
-def main() -> Dash:
-    data = DashData()
-    app = Dash(external_stylesheets=[dbc.themes.GRID])
-    app.title = "WZV League 2022-2023"
-    app.layout = create_layout(app, data)
-    return app
-
+data = DashData()
+app = Dash(external_stylesheets=[dbc.themes.GRID])
+app.title = "WZV League 2022-2023"
+app.layout = create_layout(app, data)
+server=app.server
 
 if __name__ == "__main__":
-    app = main()
-    server = app.server
     app.run()
