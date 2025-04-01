33f323787b219d3fb04be71027ce5080081f17ac
Mike Fleuren <mikefleuren@gmail.com>
2025-04-01 21:09:20 +0200

    r32

--- scraper_soccerway/gather_logic.py
+++ scraper_soccerway/gather_logic.py
@@ -184,7 +184,7 @@ def determine_matches_to_scrape(data: CompetitionData) -> pd.DataFrame:
     data.matches = data.matches.sort_values(by='Datum')
 
     max_dates_by_cluster = data.matches.groupby("Cluster", as_index=False)["Datum"].max()
-    completed_clusters = max_dates_by_cluster[max_dates_by_cluster["Datum"].dt.date <= datetime.today().date()]
+    completed_clusters = max_dates_by_cluster[max_dates_by_cluster["Datum"].dt.date < datetime.today().date()]
     played_matches = data.matches["Cluster"].isin(completed_clusters["Cluster"].unique())
 
     played_matches = data.matches["Datum"] <= datetime.today().strftime('%Y-%m-%d')
